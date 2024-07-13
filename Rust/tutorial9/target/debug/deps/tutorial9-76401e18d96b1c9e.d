1e81c7eb210d01450fd180a10f494c84fa368e70
Atharva <atharva.malik@gmail.com>
2024-07-13 20:43:05 +1000

    Finished tutorials
    Going to start a new project to train me for my driving test

---
+++ Rust/tutorial9/target/debug/deps/tutorial9-76401e18d96b1c9e.d
@@ -0,0 +1,5 @@
+C:\Atharva\Programming\Python\Python Scripts\Random-Projects\Rust\tutorial9\target\debug\deps\libtutorial9-76401e18d96b1c9e.rmeta: src\main.rs
+
+C:\Atharva\Programming\Python\Python Scripts\Random-Projects\Rust\tutorial9\target\debug\deps\tutorial9-76401e18d96b1c9e.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/tutorial9/target/debug/deps/tutorial9-d08be618b23eed5e.d
@@ -0,0 +1,5 @@
+C:\Atharva\Programming\Python\Python Scripts\Random-Projects\Rust\tutorial9\target\debug\deps\libtutorial9-d08be618b23eed5e.rmeta: src\main.rs
+
+C:\Atharva\Programming\Python\Python Scripts\Random-Projects\Rust\tutorial9\target\debug\deps\tutorial9-d08be618b23eed5e.d: src\main.rs
+
+src\main.rs:
