3096116a13e6451bc52fb4f9e6a04b7f4bc36a73
Atharva <atharva.malik@gmail.com>
2024-08-08 11:41:21 +1000

    finished the text-analyser

--- Python/text-analyser.py
+++ Python/text-analyser.py
@@ -35,13 +35,36 @@ class TextAnalyzer:
         grade_level = 0.4 * ((words / sentences) + (100 * complex_words / words))
         return grade_level
 
-    def count_syllables(self):
-        # Implement syllable counting logic
-        pass
+    def count_syllables(self, wor=""):
+        # Ans = 0
+        count = 0
+        text = self.text.split() if wor == "" else wor.split()
+        for i in text:
+            vowels = 'aeiouy'
+            word = i.lower()
+            if word[0] in vowels:
+                count += 1
+            for index in range(1, len(word)):
+                if word[index] in vowels and word[index - 1] not in vowels:
+                    count += 1
+            if word.endswith('e'):
+                count -= 1
+            if count == 0:
+                count += 1
+        return count
 
     def count_complex_words(self):
-        # Implement complex word counting logic
-        pass
+        text = self.text
+        complex_words_list = set()  # Using a set for faster lookups
+        with open('Python\complex_words.txt', 'r') as file:
+            for line in file:
+                complex_words_list.add(line.strip().lower())
+        words = text.split()
+        complex_word_count = 0
+        for word in words:
+            if self.count_syllables(word) >= 3 or word.lower() in complex_words_list:
+                complex_word_count += 1
+        return complex_word_count
 
     def frequency_analysis(self, n=10):
         words = self.text.split()
@@ -51,7 +74,7 @@ class TextAnalyzer:
         most_common = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)[:n]
         return most_common
 
-text = "This is a sample text for analysis. It contains several sentences and words."
+text = "This is a sample text that I wrote for the text-analyser program. "
 analyzer = TextAnalyzer(text)
 
 print("Word count:", analyzer.word_count())
