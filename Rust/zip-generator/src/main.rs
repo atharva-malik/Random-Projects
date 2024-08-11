145b1f8101248fc7c07cde2ea514671d40c80a44
Atharva <atharva.malik@gmail.com>
2024-08-11 13:33:01 +1000

    zip-generator: 2
    This is a failed project, I tried to get this to work, but it doesn't work and I have no idea as to why, so I am going to stop banging my head and do something else

--- Rust/zip-generator/src/main.rs
+++ Rust/zip-generator/src/main.rs
@@ -47,7 +47,76 @@ fn get_file_paths() -> Vec<String> {
         .collect()
 }
 
-fn huffman_compress(data: &mut impl Read) -> Vec<u8> {
-    // Implement Huffman compression algorithm
-    unimplemented!()
+use std::collections::BinaryHeap;
+use std::io::{Read, Write};
+
+#[derive(Eq, PartialEq)]
+struct Node {
+    freq: u32,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
 }
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.freq.cmp(&self.freq)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn huffman_compress(data: &mut impl Read) -> Vec<u8> {
+    // Read data into a byte array
+    let mut buffer = Vec::new();
+    data.read_to_end(&mut buffer).unwrap();
+
+    // Calculate frequency of each byte
+    let mut freq_map = [0u32; 256];
+    for byte in &buffer {
+        freq_map[*byte as usize] += 1;
+    }
+
+    // Create a min-heap of nodes
+    let mut heap = BinaryHeap::new();
+    for (i, freq) in freq_map.iter().enumerate() {
+        if *freq > 0 {
+            heap.push(Node {
+                freq: *freq,
+                left: None,
+                right: None,
+            });
+        }
+    }
+
+    // Build Huffman tree
+    while heap.len() > 1 {
+        let right = heap.pop().unwrap();
+        let left = heap.pop().unwrap();
+        let node = Node {
+            freq: left.freq + right.freq,
+            left: Some(Box::new(left)),
+            right: Some(Box::new(right)),
+        };
+        heap.push(node);
+    }
+
+    // Assign codes to characters
+    let root = heap.pop().unwrap();
+    let mut codes = vec![None; 256];
+    assign_codes(&root, &mut codes, &mut Vec::new());
+
+    // Encode data
+    let mut encoded_data = Vec::new();
+    // ... (implement encoding logic)
+
+    encoded_data
+}
+
+fn assign_codes(node: &Node, codes: &mut Vec<Option<Vec<bool>>>, prefix: &mut Vec<bool>) {
+    // ... (implement code assignment logic)
+}
+
